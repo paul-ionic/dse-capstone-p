5eb746ae3837b361becbf5ec56b7916fb201856e
Paul LaCaille <paul@ionic.io>
2022-02-14 09:10:58 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'dse_capstone_p',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
