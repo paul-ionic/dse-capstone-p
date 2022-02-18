85cd0090c8c9fd2af59c14098fb5cc59a7f7e88b
Paul LaCaille <paul@ionic.io>
2022-02-18 14:55:12 -0600

    set app id

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'me.lacaille.dsecapstonepaul',
   appName: 'dse_capstone_p',
   webDir: 'www',
   bundledWebRuntime: false
