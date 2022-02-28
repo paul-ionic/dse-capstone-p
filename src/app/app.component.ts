244b653f42e6dc752d387a1db1b4f5995ec423d4
Paul LaCaille <paul@ionic.io>
2022-02-28 14:13:21 -0600

    ADD ROCKETS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
+    { title: 'ROCKETS', url: '/folder/ROCKETS', icon: 'rocket' },
     { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
     { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
     { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
